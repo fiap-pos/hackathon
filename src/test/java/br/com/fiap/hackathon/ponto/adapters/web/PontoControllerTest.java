7b4b07b7802757d18fa64209affa9ad99fc03bf9
coelhos-gabi <coelhos.gabi@gmail.com>
2024-03-19 23:54:47 -0300

    Implementação teste unitário PontoController

--- src/test/java/br/com/fiap/hackathon/ponto/adapters/web/PontoControllerTest.java
+++ src/test/java/br/com/fiap/hackathon/ponto/adapters/web/PontoControllerTest.java
@@ -1,2 +1,70 @@
-package br.com.fiap.hackathon.ponto.adapters.web;public class PontoControllerTest {
+package br.com.fiap.hackathon.ponto.adapters.web;
+
+import br.com.fiap.hackathon.ponto.adapters.web.mappers.PontoMapper;
+import br.com.fiap.hackathon.ponto.core.ports.in.BuscaStatusDiaInputPort;
+import br.com.fiap.hackathon.ponto.core.ports.in.RegistraPontoInputPort;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collections;
+
+import static br.com.fiap.hackathon.ponto.utils.PontoHelper.getPontoDTO;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class PontoControllerTest {
+    private MockMvc mockMvc;
+
+    @Mock
+    RegistraPontoInputPort registraPontoInputPort;
+
+    @Mock
+    BuscaStatusDiaInputPort buscaStatusDiaInputPort;
+    PontoMapper mapper;
+    AutoCloseable mock;
+
+    @BeforeEach
+    void setUp() {
+        this.mapper = new PontoMapper();
+        mock = MockitoAnnotations.openMocks(this);
+        PontoController pontoController = new PontoController(
+                registraPontoInputPort,
+                buscaStatusDiaInputPort,
+                mapper
+        );
+
+        mockMvc = MockMvcBuilders.standaloneSetup(pontoController).build();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        mock.close();
+    }
+
+    @Test
+    void buscarPedidoPorId() throws Exception {
+        var id = 1L;
+        var pontoDTO = getPontoDTO();
+
+        when(buscaStatusDiaInputPort.buscaStatusDia()).thenReturn(Collections.singletonList(pontoDTO));
+
+        ResultActions result = mockMvc.perform(get("/ponto").contentType(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+
+        verify(buscaStatusDiaInputPort, times(1)).buscaStatusDia();
+        verifyNoMoreInteractions(buscaStatusDiaInputPort);
+    }
+
 }
--- src/test/resources/features/criar-pedido.feature
+++
@@ -1,7 +0,0 @@
-# language: pt
-Funcionalidade: Criar Pedido
-
-  Cenário: Criar Pedido com Lanche e Bebida e Cliente Cadastrados
-    Quando criar um novo pedido com cliente
-    Então o pedido é criado com sucesso com cliente apresentado
-    E deve ser apresentado com cliente
